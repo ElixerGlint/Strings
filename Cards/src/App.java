5c6588018ab488478f5ab9367ff3dd993168b8af
Elixer <everett.rumery@gmail.com>
2024-09-19 13:24:24 -0400

    asd

--- Cards/src/App.java
+++ Cards/src/App.java
@@ -1,6 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int r = (int)(Math.random()*4);
-        //asd
+        
+        int s = (int)(Math.random()*4);
+        int r = (int)(Math.random()*13); //[0-12]
+        System.out.println(r + ", " + s);
+        
     }
 }
