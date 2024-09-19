81ab8cbbed2c6abd1c93f69f4235700da9a6ceb8
Elixer <everett.rumery@gmail.com>
2024-09-19 13:38:37 -0400

    asasg

--- Cards/src/App.java
+++ Cards/src/App.java
@@ -3,25 +3,29 @@ public class App {
         
         int s = (int)(Math.random()*4);
         int r = (int)(Math.random()*13); //[0-12]
-        System.out.println(r + ", " + s);
-        
-        switch (s){
-            case 0:
+        char suit = getsuit(s);
 
-            case 1:
-
-            case 2:
-
-            case 3:
-
-
-        }
+        System.out.println(r + ", " + suit);
     }
 
 
 
-    public static void asd() {
-        
+    public static char getsuit(int n) { //uses ASCII
+        char finalvari;
+        switch (n){
+            case 0: 
+            return (char)3; //black spades
+            case 1:
+            //black club
+            return (char)4;
+            case 2:
+            return (char)5; //white heart
+            case 3:
+            return (char)6; //white diamond
+            default:
+            finalvari = 0;
+            return finalvari;
+        }
     }
 
 }
