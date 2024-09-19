d5622390319d2b500902a5fd76838d91588f44ce
Elixer <everett.rumery@gmail.com>
2024-09-19 13:48:18 -0400

    asgasg

--- Cards/src/App.java
+++ Cards/src/App.java
@@ -5,7 +5,19 @@ public class App {
         int r = (int)(Math.random()*13); //[0-12]
         char suit = getsuit(s);
 
-        System.out.println(r + ", " + suit);
+
+
+
+        //dealing out cards till you get a jack of hearts
+        System.out.println(r + 1 + ", " + suit);
+        
+        do { //1/48 odds
+            s = (int)(Math.random()*4);
+            r = (int)(Math.random()*13); //[0-12]
+            suit = getsuit(s);
+            System.out.println(r + ", " + suit);
+        } while(!(r==11&&s==0));
+        
     }
 
 
@@ -13,15 +25,14 @@ public class App {
     public static char getsuit(int n) { //uses ASCII
         char finalvari;
         switch (n){
-            case 0: 
-            return (char)3; //black spades
-            case 1:
-            //black club
+            case 0: //white heart
+            return (char)3; 
+            case 1: //white diamond
             return (char)4;
-            case 2:
-            return (char)5; //white heart
-            case 3:
-            return (char)6; //white diamond
+            case 2: //white clubs
+            return (char)5;
+            case 3: //white thing (spades)
+            return (char)6;
             default:
             finalvari = 0;
             return finalvari;
