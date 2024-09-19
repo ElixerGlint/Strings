489de70f3976097e3cdc3351bd69a547731d17cb
Elixer <everett.rumery@gmail.com>
2024-09-19 13:26:47 -0400

    bxc

--- Cards/src/App.java
+++ Cards/src/App.java
@@ -5,5 +5,23 @@ public class App {
         int r = (int)(Math.random()*13); //[0-12]
         System.out.println(r + ", " + s);
         
+        switch (s){
+            case 0:
+
+            case 1:
+
+            case 2:
+
+            case 3:
+
+
+        }
     }
+
+
+
+    public static void asd() {
+        
+    }
+
 }
