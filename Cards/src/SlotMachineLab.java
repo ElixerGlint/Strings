bbe7c4f29abaa4aa42f7d1054a8fad73b20ec54f
Elixer <everett.rumery@gmail.com>
2024-09-23 13:57:19 -0400

    asdf

--- Cards/src/SlotMachineLab.java
+++ Cards/src/SlotMachineLab.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
+
 public class SlotMachineLab {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to the casino");
+
+        System.out.println("How much money do you have: ");
+        double money = input.nextDouble();
+
         System.out.println("What is the bet amount: ");
-        double money = 0;
         double bet = input.nextDouble();
 
-        gamble();
-        //15 possibilities per wheel
-
+        gamble(money, bet);
+        // 15 possibilities per wheel
 
     }
 
-    public static void gamble() {
+    public static void gamble(double money, double bet) {
         System.out.println("gamble");
 
-        int wheel1 = (int)(Math.random() * 15);
-        int wheel2 = (int)(Math.random() * 15);
-        int wheel3 = (int)(Math.random() * 15);
+        int wheel1 = (int) (Math.random() * 15);
+        int wheel2 = (int) (Math.random() * 15);
+        int wheel3 = (int) (Math.random() * 15);
 
+        
 
         System.out.println("_________________");
         System.out.println("|   LUCKY 777   |");
@@ -27,6 +31,6 @@ public class SlotMachineLab {
         System.out.println("|   W   O   W   |");
         System.out.println("|---------------|");
         System.out.println("_________________");
-        
+
     }
 }
