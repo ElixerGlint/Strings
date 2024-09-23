388e3a4062f67094a08b4bb360e1dbd36329d704
Elixer <everett.rumery@gmail.com>
2024-09-23 14:05:49 -0400

    broken state

--- Cards/src/SlotMachineLab.java
+++ Cards/src/SlotMachineLab.java
@@ -20,17 +20,42 @@ public class SlotMachineLab {
         System.out.println("gamble");
 
         int wheel1 = (int) (Math.random() * 15);
+        // 9 Ws, 3 Zs, 3 Xs
+        String letter1 = wheel1letter(wheel1);
+        String letter2 = wheel2letter(wheel2);
         int wheel2 = (int) (Math.random() * 15);
         int wheel3 = (int) (Math.random() * 15);
 
-        
+
 
         System.out.println("_________________");
         System.out.println("|   LUCKY 777   |");
         System.out.println("|---------------|");
-        System.out.println("|   W   O   W   |");
+        System.out.println("|   " + letter1 + "   " + letter2 + "   W   |");
         System.out.println("|---------------|");
         System.out.println("_________________");
 
+    }
+
+    public static String wheel1letter(int number) {
+        String letter1 = "ERROR"; //prints error if it breaks
+        if(number < 9 && number >= 0) {
+            letter1 = "W";
+        }
+        if(number < 12 && number >= 9) {
+            letter1 = "X";
+        }
+        if(number < 15 && number >= 12) {
+            letter1 = "Z";
+        }
+        return letter1;
+    }
+    public static String wheel2letter(int number) {
+        String letter1 = "ERROR"; //prints error if it breaks
+        if(number < 5 && number >= 0) {
+            letter1 = "I";
+        }
+        //broken rn
+        return letter1;
     }
 }
