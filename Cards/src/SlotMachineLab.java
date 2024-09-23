58cf3210c1301049ee0631f5d5b0d8aa6f7a48c1
Elixer <everett.rumery@gmail.com>
2024-09-23 13:47:09 -0400

    asf

---
+++ Cards/src/SlotMachineLab.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SlotMachineLab {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the casino");
+        System.out.println("What is the bet amount: ");
+        double money = 0;
+        double bet = input.nextDouble();
+
+        
+
+    }
+
+    public static void gamble() {
+        System.out.println("gamble");
+
+    }
+}
