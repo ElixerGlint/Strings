7ef16da253bcce26000f3519402338314224211d
Elixer <everett.rumery@gmail.com>
2024-09-23 13:52:29 -0400

    asfasg

--- Cards/src/SlotMachineLab.java
+++ Cards/src/SlotMachineLab.java
@@ -7,12 +7,26 @@ public class SlotMachineLab {
         double money = 0;
         double bet = input.nextDouble();
 
-        
+        gamble();
+        //15 possibilities per wheel
+
 
     }
 
     public static void gamble() {
         System.out.println("gamble");
 
+        int wheel1 = (int)(Math.random() * 15);
+        int wheel2 = (int)(Math.random() * 15);
+        int wheel3 = (int)(Math.random() * 15);
+
+
+        System.out.println("_________________");
+        System.out.println("|   LUCKY 777   |");
+        System.out.println("|---------------|");
+        System.out.println("|   W   O   W   |");
+        System.out.println("|---------------|");
+        System.out.println("_________________");
+        
     }
 }
