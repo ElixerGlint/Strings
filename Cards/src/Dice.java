7aeec70bad382fab44139756aff1610be78206bf
Elixer <everett.rumery@gmail.com>
2024-09-23 13:29:28 -0400

    asfgasg

--- Cards/src/Dice.java
+++ Cards/src/Dice.java
@@ -1,20 +1,45 @@
+import java.util.Scanner;
 public class Dice {
     public static void main(String[] args) throws Exception {
+        double money = 100.0;
+        double bet;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Bet: ");
+        bet = input.nextDouble();
 
 
-        System.out.println("hi");
 
+        int roll = (int)(Math.random()*6) + 1 + (int)(Math.random()*6) + 1;
+        if(roll == 7 || roll == 11) {
+            System.out.println("Congrats on winning!");
+            money += bet;
+        }
 
-
-
-        int die1 = (int)(Math.random()%6);
-        int die2 = (int)(Math.random()%6);
+        else if (roll == 2 || roll == 3 || roll == 12) {
+            System.out.println("Sorry, you lost...");
+            money -= bet;
+        }
+        int newroll;
+        do {
+            newroll = (int)(Math.random()*6) + 1 + (int)(Math.random()*6) + 1;
+        }while(newroll !=7 && newroll != roll);
+        if(newroll == 7) {
+            System.out.println("You lost!");
+            money -= bet;
+        }
+        else {
+            System.out.println("Congrats on winning!");
+            money += bet;
+        }
         
 
-        //7, 11 to win
-        //2, 3, 
+        //7, 11 to insta win
+        //2, 3, 12 insta lose
 
-
+        //else roll until
+        //7 = insta lose
+        //match first == insta win
 
     }
 }
