9d2e8e94a22ee573f8bcfb2a7e061056ea2649ae
Elixer <everett.rumery@gmail.com>
2024-09-23 13:44:13 -0400

    asgasg

--- Cards/src/Dice.java
+++ Cards/src/Dice.java
@@ -2,41 +2,17 @@ import java.util.Scanner;
 public class Dice {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to the cacino");
-        System.out.println("What is the bet amount: ");
-        double bet = input.nextDouble();
-        System.out.println("How many rounds would you like to simulate");
-        int count = input.nextInt();
+        //System.out.println("Welcome to the cacino");
+        //System.out.println("What is the bet amount: ");
+        //double bet = input.nextDouble();
+        //System.out.println("How many rounds would you like to simulate");
+        //int count = input.nextInt();
 
-        System.out.println(simulate(bet,count));
+        System.out.println(simulate(10,10));
 
     }
 
-    public static double simulate(double bet, double count) {
-        double money = 0;
-        for(int i = 0; i< count; i++) {
-        int roll = (int)(Math.random()*6) + 1 + (int)(Math.random()*6) + 1;
-        if(roll == 7 || roll == 11) {
-            money += bet;
-        }
-
-        else if (roll == 2 || roll == 3 || roll == 12) {
-            //loss
-            money -= bet;
-        }
-        int newroll;
-        do {
-            newroll = (int)(Math.random()*6) + 1 + (int)(Math.random()*6) + 1;
-        }while(newroll !=7 && newroll != roll);
-        if(newroll == 7) {
-            //loss
-            money -= bet;
-        }
-        else {
-            //win
-            money += bet;
-        }
-    }
-        return money;
+    public static void simulate() {
+        System.out.println("gamble");
     }
 }
