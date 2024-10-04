843534aab2857700979af6861bfc08e21f1e7d51
Elixer <everett.rumery@gmail.com>
2024-10-04 13:16:18 -0400

    asd

--- NewGame/src/Strings.java
+++ NewGame/src/Strings.java
@@ -2,7 +2,8 @@ public class Strings {
     public static void main(String[] args) throws Exception {
 	String str = "Hello World";
     String str2 = "Hello World";
-
+        
+    System.out.println(str==str2);//same object, therefore it is true
 
 
 	
