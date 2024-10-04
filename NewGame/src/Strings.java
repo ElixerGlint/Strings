979ac3b1d20b2872becef52bd234a79a15cfa869
Elixer <everett.rumery@gmail.com>
2024-10-04 13:30:31 -0400

    gghhj

--- NewGame/src/Strings.java
+++ NewGame/src/Strings.java
@@ -1,18 +1,23 @@
 public class Strings {
     public static void main(String[] args) throws Exception {
+        /* 
         String str = "Hello World";
         String str2 = "Hello World";
         System.out.println(str == str2);// same object, therefore it is true
         String str3 = str.substring(0, 4) + "o " + str2.substring(6);
         System.out.println(str == str3); // should be the same, but it is a different object
         System.out.println(str.equals(str3)); // same value, therefore true
-
         // compareTo
         String A = "A";
         String B = "B";//a - b
         System.out.println(A.compareTo(B)); //shoud be -1, based on numerical values of the characters
         System.out.println(str.compareTo("Hey")); //-13 because first difference, then kicks out
         System.out.println("A".compareTo("a")); //uppercase letters come before lowercase in unicode
+        System.out.println("Cat".compareTo("Cataract")); //ran out of chars to compare, so it compares their counts
+        */
+
+
+
 
     } // close main
 } // close class
