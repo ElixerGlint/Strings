8f50d97af06406b63ff86c57a111b032ad3ffd60
Elixer <everett.rumery@gmail.com>
2024-10-04 13:20:42 -0400

    g

--- NewGame/src/Strings.java
+++ NewGame/src/Strings.java
@@ -1,14 +1,15 @@
 public class Strings {
     public static void main(String[] args) throws Exception {
-	String str = "Hello World";
-    String str2 = "Hello World";
-    System.out.println(str==str2);//same object, therefore it is true
-    String str3 = str.substring(0,4);
+        String str = "Hello World";
+        String str2 = "Hello World";
+        System.out.println(str == str2);// same object, therefore it is true
+        String str3 = str.substring(0, 4) + "o " + str2.substring(6);
+        System.out.println(str == str3); // should be the same, but it is a different object
+        System.out.println(str.equals(str3)); // same value, therefore true
 
-	
+        // compareTo
+        String A = "A";
+        String B = "B";
 
-
-
-
-    } //close main
-} //close class
+    } // close main
+} // close class
